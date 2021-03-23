a0c5ca66795e60708ba67e6af40c1d9353d9b7d1
lari-nyan <75284127+lari-nyan@users.noreply.github.com>
2021-03-22 22:23:20 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,90 @@
+// Initialize your app
+var myApp = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	panel:{
+		swipe:true
+	},
+	toolbar: {
+		hideOnPageScroll: true,
+	  },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Airbnb',
+	dialog: {
+		title: 'Airbnb',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/casa/',
+    	url: 'casa.html',
+    	name: 'casa',
+  		},
+		{
+		path: '/habitacion/',
+    	url: 'habitacion.html',
+    	name: 'habitacion',
+  		},
+		{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		},
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+var notificationFull = myApp.notification.create({
+        icon: '<i class="material-icons">shopping_cart</i>',
+        title: 'eShop',
+        subtitle: 'Add to Cart',
+        text: 'Your product was added successfully',
+        closeTimeout: 5000,
+      });
+
+
+$$(document).on('click','#add-cart', function () {
+	console.log('add');
+	notificationFull.open();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
